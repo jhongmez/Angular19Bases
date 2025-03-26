d084397240be92adc70dc70084e83e1456929512
Jhon Gómez Marín <gmezjhon@gmail.com>
2025-03-25 19:22:02 -0500

    ✅ BI-0004 (HeroPage): Use signals and pipes with heroes

--- src/app/pages/hero/hero-page.component.ts
+++ src/app/pages/hero/hero-page.component.ts
@@ -1,9 +1,33 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
+import { UpperCasePipe } from '@angular/common';
+
 
 @Component({
 	selector: 'app-hero-page',
 	templateUrl: './hero-page.component.html',
+	imports: [UpperCasePipe],
 })
 export class HeroPageComponent {
 
+	public name = signal('Ironman');
+	public age = signal(45);
+
+	getHeroDescription() {
+		return `${this.name} - ${this.age}`;
+	}
+
+	changeHero() {
+		// * La forma correcta de actualizar un signal es con el método update
+		this.name.set('Spiderman');
+		this.age.set(22);
+	}
+
+	resetForm() {
+		this.name.set('Ironman');
+		this.age.set(45);
+	}
+
+	changeAge() {
+		this.age.set(60);
+	}
 }
