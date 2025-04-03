291b20ac1a528d916f24b6d05056390f1eb39267
Jhon Gómez Marín <gmezjhon@gmail.com>
2025-04-02 19:33:59 -0500

    ✅ BI-0012(DragonBallPage): Add function addCharacter and resetFields

--- src/app/pages/dragonball-page/dragonball-page.component.ts
+++ src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, Signal, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 interface Character {
 	id: number;
@@ -14,6 +14,7 @@ interface Character {
 export class DragonballPageComponent {
 
 	name = signal('Gohan');
+	power = signal(56000);
 
 	characters = signal<Character[]>([
 		{ id: 1, name: 'Goku', power: 90001 },
@@ -21,5 +22,23 @@ export class DragonballPageComponent {
 		{ id: 3, name: 'Trunks', power: 30000 },
 		{ id: 4, name: 'Piccolo', power: 40000 },
 	]);
+
+	addCharacter() {
+		if( !this.name() || !this.power() || this.power() <= 0 ) return;
+
+		const newCharacter: Character = {
+			id: this.characters().length + 1,
+			name: this.name(),
+			power: this.power()
+		}
+
+		// * La forma correcta de actualizar un signal es con el método update
+		this.characters.update( (listCharacters) => [...listCharacters,newCharacter]);
+	}
+
+	resetFields() {
+		this.name.set('');
+		this.power.set(0);
+	}
 
 }
