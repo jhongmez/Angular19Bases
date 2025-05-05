d0b66e3f6e8ddec22f8198778ea2a1b5445d3bfa
Jhon Gómez Marín <gmezjhon@gmail.com>
2025-05-05 06:39:32 -0500

    BI-0016 (Dragonball, DragonballService): Add new service

--- src/app/pages/dragonball-super/dragonball-super.component.ts
+++ src/app/pages/dragonball-super/dragonball-super.component.ts
@@ -1,12 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
 import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
-
-interface Character {
-	id: number;
-	name: string;
-	power: number;
-}
+import { DragonballService } from '../../services/dragonball.service';
 
 @Component({
   selector: 'app-dragonball-super',
@@ -15,13 +10,6 @@ interface Character {
 })
 export class DragonballSuperComponent {
 
-	addCharacter( newCharacter: Character) {
-		this.characters.update( (listCharacters) => [...listCharacters,newCharacter]);
-	}
-
-	characters = signal<Character[]>([
-		{ id: 1, name: 'Goku', power: 90001 },
-		{ id: 2, name: 'Vegeta', power: 70000 },
-	]);
+	public _dragonballSvc = inject(DragonballService);
 
 }
---
+++ src/app/services/dragonball.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, signal } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+
+@Injectable({providedIn: 'root'})
+export class DragonballService {
+
+	addCharacter( newCharacter: Character) {
+		this.characters.update( (listCharacters) => [...listCharacters,newCharacter]);
+	}
+
+	characters = signal<Character[]>([
+		{ id: 1, name: 'Goku', power: 90001 },
+		{ id: 2, name: 'Vegeta', power: 70000 },
+	]);
+
+}
